b3b3b2562fada8375a07ba026b4e285ab23eb178
Robin Kraft <rkraft4@gmail.com>
2012-11-09 14:35:28 -0800

    Fixed lein-emr specs in project.clj

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
   :resources-path "resources"
   :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                                   [midje-cascalog "0.4.0"]]}
-             :plugins [[lein-midje "2.0.0-SNAPSHOT"]
-                       [lein-emr "0.1.0-SNAPSHOT"]]}
-  :plugins [[lein-swank "1.4.4"]]
+             :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}
+  :plugins [[lein-swank "1.4.4"]
+            [lein-emr "0.1.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cascalog "1.9.0"]
                  [backtype/dfs-datastores "1.1.3"]
