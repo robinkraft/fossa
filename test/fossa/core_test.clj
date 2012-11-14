f89937f29166485a790307d0d06e80dcc533dcc7
Robin Kraft <robin@whizbangsystems.net>
2012-11-13 18:46:25 -0800

    Handling missing data, trailing zeros, etc. per #19 #20 #21

--- test/fossa/core_test.clj
+++ test/fossa/core_test.clj
@@ -5,10 +5,14 @@
 (fact
   "Test read-occurrences using sample dataset"
   (read-occurrences)
-  => (produces-some [["Acidobacteria" "41.5265650" "-70.6707620" "242135147" "" "2007" "1"]]))
+  => (produces-some [["Passer domesticus" "999999999" "-40.8747" "170.851" "" "2007" "6"]
+                     ["Passer domesticus" "111111111" "-40.8747" "170.851" "10" "2007" ""]
+                     ["Passer domesticus" "333333333" "-40.8747283" "170.851" "" "2007" ""]
+                     ["Passer domesticus" "444444444" "-40.8747283" "170.851" "" "2007" ""]
+                     ["Passer domesticus" "222222222" "-40.8747283" "170.851" "10" "2007" ""]]))
 
 (fact
   "Test parse-occurrence-data"
   (parse-occurrence-data)
-  => (produces [["Acidobacteria" "INSERT INTO gbif_points (name, occids, precision, year, month, season, the_geom_multipoint) values ('Acidobacteria', '{\"244664083\", \"244662763\", \"100000000,500000000\", \"244662123\", \"242136127\", \"242135147\", \"242135541\", \"244666043\", \"244661001\", \"244661391\", \"242135095\"}', '{\"\", \"\", \",\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"}', '{\"2007\", \"2007\", \"2008,2010\", \"2008\", \"2007\", \"2007\", \"2007\", \"1992\", \"2008\", \"2007\", \"2005\"}', '{\"6\", \"4\", \"10,3\", \"9\", \"3\", \"1\", \"6\", \"4\", \"5\", \"8\", \"3\"}', '{\"S winter\", \"S fall\", \"S spring,S fall\", \"N fall\", \"N spring\", \"N winter\", \"N summer\", \"N spring\", \"N summer\", \"N fall\", \"N spring\"}', ST_GeomFromText('MULTIPOINT (170.8510000 -40.8747000, -175.5532000 -43.2880000, 0.0000000 0.0000000, 73.8830600 15.5097220, 35.4166667 31.4166667, -70.6707620 41.5265650, -70.6336000 41.5762330, 19.5833340 47.1666680, 6.1444330 53.4902700, -140.0880000 73.9670000, 0.8966667 9.9316667)', 4326))"]]))
+  => (produces [["Passer domesticus" "INSERT INTO gbif_points (name, occids, precision, year, month, season, the_geom_multipoint) values ('Passer domesticus', '{\"999999999,111111111\", \"333333333,444444444,222222222\"}', '{\",10\", \",,10\"}', '{\"2007,2007\", \"2007,2007,2007\"}', '{\"6,\", \",,\"}', '{\"4,\", \",,\"}', ST_GeomFromText('MULTIPOINT (170.851 -40.8747, 170.851 -40.8747283)', 4326))"]]))
 
