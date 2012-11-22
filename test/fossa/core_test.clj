0b01e18d30a06c23cf603d4cac69f51720d593d4
Robin Kraft <rkraft4@gmail.com>
2012-11-21 20:32:57 -0800

    Updated tests to reflect more generalized method of handling new tuple fields

--- test/fossa/core_test.clj
+++ test/fossa/core_test.clj
@@ -20,7 +20,7 @@
   => (produces-some
       [["UPDATE gbif_points SET the_geom_multipoint = 'MULTIPOINT (0 0, -70.6336 41.576233, 6.144433 53.49027)' WHERE name = 'Acidobacteria';"]
        ["UPDATE gbif_points SET occids = '{\"100000000,500000000\", \"242135541\", \"244661001\"}' WHERE name = 'Acidobacteria';"]
-       ["UPDATE gbif_points SET precision = '{\",\", \"\", \"\"}' WHERE name = 'Acidobacteria';"]
-       ["UPDATE gbif_points SET year = '{\"2008,2010\", \"2007\", \"2008\"}' WHERE name = 'Acidobacteria';"]
-       ["UPDATE gbif_points SET month = '{\"10,3\", \"6\", \"5\"}' WHERE name = 'Acidobacteria';"]
-       ["UPDATE gbif_points SET season = '{\"5,7\", \"2\", \"2\"}' WHERE name = 'Acidobacteria';"]]))
+       ["UPDATE gbif_points SET season = '{\"5,7\", \"2\", \"2\"}' WHERE name = 'Acidobacteria';"]
+       ["UPDATE gbif_points SET precisions = '{\",\", \"\", \"\"}' WHERE name = 'Acidobacteria';"]
+       ["UPDATE gbif_points SET years = '{\"2008,2010\", \"2007\", \"2008\"}' WHERE name = 'Acidobacteria';"]
+       ["UPDATE gbif_points SET months = '{\"10,3\", \"6\", \"5\"}' WHERE name = 'Acidobacteria';"]]))
